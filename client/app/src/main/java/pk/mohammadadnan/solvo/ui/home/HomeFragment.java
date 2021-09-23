3020f6ca268aa9c39a3f76b3727fc93ca6a5faa3
adnan-creator <adnanfreelancerr@gmail.com>
2021-09-23 18:18:10 +0500

    First Push
    Added Basic UI Functionality.

---
+++ client/app/src/main/java/pk/mohammadadnan/solvo/ui/home/HomeFragment.java
@@ -0,0 +1,33 @@
+package pk.mohammadadnan.solvo.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import pk.mohammadadnan.solvo.R;
+import pk.mohammadadnan.solvo.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        View root = FragmentHomeBinding.inflate(inflater, container, false).getRoot();
+
+
+        return root;
+    }
+
+}
---
+++ client/app/src/main/java/pk/mohammadadnan/solvo/ui/interests/InterestsFragment.java
@@ -0,0 +1,46 @@
+package pk.mohammadadnan.solvo.ui.interests;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+
+import pk.mohammadadnan.solvo.databinding.FragmentInterestsBinding;
+
+public class InterestsFragment extends Fragment {
+
+    private InterestsViewModel dashboardViewModel;
+    private FragmentInterestsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                new ViewModelProvider(this).get(InterestsViewModel.class);
+
+        binding = FragmentInterestsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textDashboard;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
