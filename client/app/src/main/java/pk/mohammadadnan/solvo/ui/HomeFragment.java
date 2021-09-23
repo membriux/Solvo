fa79fb0d3754a58aca8b115e23787b4b9da9d2ce
adnan-creator <adnanfreelancerr@gmail.com>
2021-09-24 04:37:16 +0500

    All changes synced

--- client/app/src/main/java/pk/mohammadadnan/solvo/ui/HomeFragment.java
+++ client/app/src/main/java/pk/mohammadadnan/solvo/ui/HomeFragment.java
@@ -1,29 +1,26 @@
-package pk.mohammadadnan.solvo.ui.home;
+package pk.mohammadadnan.solvo.ui;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import pk.mohammadadnan.solvo.R;
 import pk.mohammadadnan.solvo.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-    private int i = 0;
-
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
 
-        View root = FragmentHomeBinding.inflate(inflater, container, false).getRoot();
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         return root;
     }
