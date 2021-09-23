1c0e19c117e7cb01853be829ba28d8e7140249ee
Guillermo <Sanguillermo98@gmail.com>
2021-09-23 17:18:03 -0400

    Finished GET /problems on client

--- client/app/src/main/java/pk/mohammadadnan/solvo/MainActivity.java
+++ client/app/src/main/java/pk/mohammadadnan/solvo/MainActivity.java
@@ -15,7 +15,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import pk.mohammadadnan.solvo.databinding.ActivityMainBinding;
-import pk.mohammadadnan.solvo.models.Post;
+import pk.mohammadadnan.solvo.models.Problem;
 import pk.mohammadadnan.solvo.network.APIClient;
 import pk.mohammadadnan.solvo.network.GetDataService;
 import pk.mohammadadnan.solvo.ui.home.HomeViewModel;
@@ -26,9 +26,7 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
-
-    private List<Post> dataList;
+    private List<Problem> dataList;
     private Context context;
 
     @Override
@@ -44,28 +42,29 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-
-
+        /* ––––- ––––– */
+        /* ––––- API Client ––––– */
+        /* ––––- ––––– */
         /*Create handle for the RetrofitInstance interface*/
         GetDataService service = APIClient.getRetrofitInstance().create(GetDataService.class);
 
-        Call<List<Post>> call = service.getAllPhotos();
-        call.enqueue(new Callback<List<Post>>() {
+        Call<List<Problem>> call = service.getAllProblems();
+        call.enqueue(new Callback<List<Problem>>() {
 
             @Override
-            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+            public void onResponse(Call<List<Problem>> call, Response<List<Problem>> response) {
 //                progressDoalog.dismiss();
 //                generateDataList(response.body());
-                System.out.println("Got posts");
+                System.out.println("Got Problems");
                 System.out.println(response.body());
 
             }
 
             @Override
-            public void onFailure(Call<List<Post>> call, Throwable t) {
+            public void onFailure(Call<List<Problem>> call, Throwable t) {
 //                progressDoalog.dismiss();
                 Toast.makeText(MainActivity.this,
-                        "Error getting list of posts",
+                        "Error getting list of Problems",
                         Toast.LENGTH_SHORT).show();
             }
         });
